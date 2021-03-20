507ce0fda531bf13160bb016788aeb7be8d541e2
EVANDRO SIQUEIRA SIQUER SIS - SOLUÇÕES E SISTEMAS <evandrosiquer@hotmail.com>
2021-03-19 22:26:42 -0300

    Criação projeto/exercício do curso de VueJs.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <TheHeader />
+    <router-view class="container"></router-view>
   </div>
 </template>
 
+<script>
+import TheHeader from "./components/TheHeader";
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  margin: 0px;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  color: #234;
 }
 
-#nav {
-  padding: 30px;
+ul {
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
 }
 
-#nav a {
+a {
   font-weight: bold;
-  color: #2c3e50;
+  text-decoration: none;
+  color: #234;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+img {
+  max-width: 100%;
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+a.router-link-active {
+  color: #4b8;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+
+.v-enter-active {
+  transition: all 0.7s;
+}
+
+.conteudo {
+  display: grid;
+  grid-template-columns: 1fr minmax(200px, 400px);
+  grid-gap: 30px;
+}
+
+@media screen and (max-width: 400px) {
+  .conteudo {
+    display: block;
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,45 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/views/Home.vue";
+import Contato from "@/views/Contato.vue";
+import Cursos from "@/views/Cursos.vue";
+import Curso from "@/views/Curso.vue";
+import Aula from "@/views/Aula.vue";
 
-Vue.use(VueRouter)
+Vue.use(Router);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
+export default new Router({
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/contato",
+      name: "contato",
+      component: Contato,
+    },
+    {
+      path: "/cursos",
+      name: "cursos",
+      component: Cursos,
+    },
+    {
+      path: "/cursos/:curso",
+      name: "curso",
+      component: Curso,
+      props: true,
+      children: [
+        {
+          path: ":aula",
+          name: "aula",
+          component: Aula,
+          props: true,
+        },
+      ],
+    },
+  ],
+});
